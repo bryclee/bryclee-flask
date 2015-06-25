88ef77e566e25708609276465d5a3aeb5f2e0869
Bryan Lee <pygmize.me@gmail.com>
2015-06-24 22:34:14 -0700

    [feature] Adds changeName function to rotate name  - Additionally adds favicon

--- bryclee/static/name.js
+++ bryclee/static/name.js
@@ -1,24 +1,64 @@
-var header = document.querySelector('#header');
-var subheader = document.querySelector('#subheader');
-header.innerHTML = '';
-subheader.innerHTML = '';
-var nameString = 'bryclee';
+(function() {
+	var header = document.querySelector('#header');
+	var subheader = document.querySelector('#subheader');
+	header.innerHTML = '';
+	subheader.innerHTML = 'Site in progress';
+	var nameString = 'bryclee';
 
-for (var i = 0; i < nameString.length; i++) {
-	(function() {
-		var j = i;
+	var headerWords = ['bl', 'bryclee', 'bryan c lee', 'b a cat'];
+	var characterNodes = [];
+
+	var changeHeaderWord = function(newWord) {
+		var newWordLetters = {};
+		var newWordNodes = [];
+		var tempNodeStorage = [];
+		var removeNodeStorage = [];
+		var i, pos, node;
+		for (i = 0; i < newWord.length; i++) {
+			newWordLetters[newWord[i]] = newWordLetters[newWord[i]] || [];
+			newWordLetters[newWord[i]].push(i);
+		}
+		while (node = characterNodes.pop()) {
+			if (newWordLetters[node.letter]) {
+				pos = newWordLetters[node.letter].pop();
+				newWordNodes[pos] = node.node;
+				if (!newWordLetters[node.letter].length) {
+					delete newWordLetters[node.letter];
+				}
+				tempNodeStorage.push(node);
+			} else {
+				removeNodeStorage.push(node);
+				node.node.className = 'letter exit';
+			}
+		}
+		for (i = 0; i < newWord.length; i++) {
+			if (!newWordNodes[i]) {
+				node = document.createElement('span');
+				node.textContent = node.innerText = newWord[i];
+				node.className = 'letter enter';
+				header.appendChild(node);
+				characterNodes.push({letter: newWord[i], node: node});
+				newWordNodes[i] = node;
+			}
+			newWordNodes[i].style.left = i * 50 + 'px';
+		}
+		while (node = tempNodeStorage.pop()) {
+			characterNodes.push(node);
+		}
 		window.setTimeout(function() {
-			var newChar = document.createElement('span');
-			newChar.appendChild(document.createTextNode(nameString.charAt(j)));
-			newChar.className = 'letter enter';
-			header.appendChild(newChar);
-		}, (j + 1) * 200);
-	})();
-}
+			var node;
+			while (node = removeNodeStorage.pop()) {
+				header.removeChild(node.node);
+			}
+		}, 800);
+	};
 
-window.setTimeout(function() {
-	var span = document.createElement('span');
-	span.appendChild(document.createTextNode('coming soon'));
-	span.className = 'letter enter';
-	subheader.appendChild(span);
-}, (i + 1) * 200 + 500);
+	var setNextWord = function(index, delay) {
+		changeHeaderWord(headerWords[index]);
+		window.setTimeout(function() {
+			setNextWord((index + 1) % headerWords.length, delay);
+		}, delay);
+	};
+
+	setNextWord(0, 2000)
+})();
