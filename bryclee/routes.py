94816f5a78fb127b5a33b7c59cc396597ca07e14
Bryan Lee <pygmize.me@gmail.com>
2015-07-28 00:16:03 -0700

    [feature] Makes header banner a link to index page

--- bryclee/routes.py
+++ bryclee/routes.py
@@ -1,6 +1,13 @@
+import os
 from bryclee import app
-from flask import request, render_template
+from flask import request, render_template, jsonify
 
 @app.route('/')
-def hello():
+def index():
 	return render_template('index.html', name='visitor')
+
+@app.route('/posts/<id>', methods=['GET'])
+def get_post(id):
+	return jsonify(tag="intro",
+					id=id,
+					text="Hello, this is a normal post.")
--- bryclee/static/name.js
+++ bryclee/static/name.js
@@ -1,11 +1,12 @@
 (function() {
 	var header = document.querySelector('#header');
-	header.innerHTML = '';
+	var headerLetters = document.querySelector('#header-letters');
+	headerLetters.innerHTML = '';
 
 	var headerWords = [
 		'bl', 'bryclee', 'bryan c lee', 'cats are cool', 'so are dogs'
 	];
-	var characterNodes = [];
+	var characterNodes = []; //This array holds the current character nodes
 
 	var changeHeaderWord = function(newWord) {
 		var newWordLetters = {};
@@ -13,6 +14,8 @@
 		var tempNodeStorage = [];
 		var removeNodeStorage = [];
 		var i, pos, node;
+		//Create the letter counts for the new word to compare to the current word,
+		//storing the index position that each letter is found at.
 		for (i = newWord.length - 1; i >= 0; i--) {
 			newWordLetters[newWord[i]] = newWordLetters[newWord[i]] || [];
 			newWordLetters[newWord[i]].push(i);
@@ -20,6 +23,9 @@
 		while (node = characterNodes.pop()) {
 			tempNodeStorage.push(node);
 		}
+		//Compare the current letters to the new word letters, marking the nodes that
+		//will not be used in the new word, and assigning the new index to the nodes
+		//that will be reused.
 		while (node = tempNodeStorage.pop()) {
 			if (newWordLetters[node.letter]) {
 				pos = newWordLetters[node.letter].pop();
@@ -33,25 +39,32 @@
 				node.node.className = 'letter exit';
 			}
 		}
+		//Iterate over the new word, creating nodes for new letters when needed,
+		//and reassigning the left value to reposition each of the letters
 		for (i = 0; i < newWord.length; i++) {
 			if (!newWordNodes[i]) {
 				node = document.createElement('span');
 				node.textContent = node.innerText = newWord[i];
 				node.className = 'letter enter';
-				header.appendChild(node);
+				headerLetters.appendChild(node);
 				characterNodes.push({letter: newWord[i], node: node});
 				newWordNodes[i] = node;
 			}
 			newWordNodes[i].style.left = i * 50 + 'px';
 		}
+		//Set width of parent for the anchor tag to properly resize
+		header.style.width = newWord.length * 50 + 'px';
+
+		//Clean up nodes that were marked for removal after the exit animation has finished playing
 		window.setTimeout(function() {
 			var node;
 			while (node = removeNodeStorage.pop()) {
-				header.removeChild(node.node);
+				headerLetters.removeChild(node.node);
 			}
 		}, 800);
 	};
 
+	//Cycle through the words in the headerWords array
 	var setNextWord = function(index, delay) {
 		changeHeaderWord(headerWords[index]);
 		window.setTimeout(function() {
@@ -59,5 +72,5 @@
 		}, delay);
 	};
 
-	setNextWord(0, 2000)
+	setNextWord(0, 2000);
 })();
